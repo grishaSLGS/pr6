eb7f33844e164c4e3fd0c1bca1bfe483a6e9903f
grishaSLGS <grisha.kireev.08@mail.ru>
2024-12-25 21:49:33 +0300

    commit 1

--- пр5_Киреев_Григорий_ИС-23-Т2.py
+++ пр5_Киреев_Григорий_ИС-23-Т2.py
@@ -1,29 +1,39 @@
 class Task:
-def init (self, title):
-self.title = title
-self.completed = False
-def complete(self):
-self.completed = True
-def str (self):
-status = "✓" if self.completed else "✗"
-return f"{status} {self.title}"
+class Task:
+    def __init__(self, title):  
+        self.title = title
+        self.completed = False
+
+    def complete(self):
+        self.completed = True
+
+    def __str__(self): 
+        status = "✓" if self.completed else "✗"
+        return f"{status} {self.title}"
+
 class TaskScheduler:
-def init (self):
-self.tasks = []
-def add_task(self, title):
-task = Task(title)
-self.tasks.append(task)
-def remove_task(self, index):
-if 0 <= index < len(self.tasks):
-del self.tasks[index]
-def complete_task(self, index):
-if 0 <= index < len(self.tasks):
-self.tasks[index].complete()
-def show_tasks(self):
-for i, task in enumerate(self.tasks):
-print(f"{i}: {task}")
-if name == " main ":
-scheduler = TaskScheduler()
-scheduler.add_task("Купить молоко")
-scheduler.add_task("Сделать домашнее задание")
-scheduler.show_tasks()
+    def __init__(self): 
+        self.tasks = []
+
+    def add_task(self, title):
+        task = Task(title)
+        self.tasks.append(task)
+
+    def remove_task(self, index):
+        if 0 <= index < len(self.tasks):
+            del self.tasks[index]
+
+    def complete_task(self, index):
+        if 0 <= index < len(self.tasks):
+            self.tasks[index].complete()
+
+    def show_tasks(self):
+        for i, task in enumerate(self.tasks):
+            print(f"{i}: {task}")
+
+if __name__ == "__main__":  
+    scheduler = TaskScheduler()
+    scheduler.add_task("Купить молоко")
+    scheduler.add_task("Сделать домашнее задание")
+    scheduler.show_tasks()
+
